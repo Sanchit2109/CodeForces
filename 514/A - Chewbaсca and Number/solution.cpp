1343ad4c94a7136a8471babded661910b241cc34
Sanchit Panwar <166299254+Sanchit2109@users.noreply.github.com>
2026-03-17 22:19:47 +0530

    Add Chewbaсca and Number [A] from Codeforces

---
+++ 514/A - Chewbaсca and Number/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    string s;    cin>>s;    for(int i=0;i<s.length();i++){        int d=s[i]-'0';        int inv=9-d;        if(i==0&&inv==0) continue;        if(inv<d) s[i]=char(inv+'0');    }    cout<<s;}
